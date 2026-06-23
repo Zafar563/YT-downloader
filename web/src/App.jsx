00ab4a43604cd4c352b52ce98de74da530bed341
Zafar563 <zafartohirov688@gmail.com>
2026-06-23 16:08:19 +0500

    first release

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -96,7 +96,7 @@ function App() {
   return (
     <div className="container">
       <header className="header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <h1>YT & Instagram Downloader</h1>
+        <h1>YT & Instagram Downloader first release</h1>
       </header>
 
       <div className="input-group">
